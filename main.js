0d33d0cb1aad5d394394971404f1f29d3c36ad8b
SarthakChoyal <94727252+SarthakChoyal@users.noreply.github.com>
2021-11-20 10:12:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function add(){
+    var result = 
+    Number(document.getElementById("firstNumber").value)+Number(document.getElementById("secondNumber").value)
+    window.alert(result)
+}
+function subtract(){
+    var result = 
+    Number(document.getElementById("firstNumber").value)-Number(document.getElementById("secondNumber").value)
+    window.alert(result)
+}
+function multiply(){
+    var result = 
+    Number(document.getElementById("firstNumber").value)*Number(document.getElementById("secondNumber").value)
+    window.alert(result)
+}
+function divide(){
+    var result = 
+    Number(document.getElementById("firstNumber").value)/Number(document.getElementById("secondNumber").value)
+    window.alert(result)
+}
